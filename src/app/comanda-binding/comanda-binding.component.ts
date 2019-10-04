e27fbcd997bcb5c32ed5f2d8456fe48af13c86c5
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-10-04 20:52:51 -0300

    Exercicio-7

--- src/app/comanda-binding/comanda-binding.component.ts
+++ src/app/comanda-binding/comanda-binding.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'has-comanda-binding',
@@ -8,11 +8,22 @@ import { FormBuilder,FormGroup } from '@angular/forms';
 })
 export class ComandaBindingComponent implements OnInit {
 
+    formPizzaria: FormGroup;
+    
   constructor(public formBuilder   :   FormBuilder) { }
  
    
   ngOnInit() {
-    
+    this.formPizzaria = this.formBuilder.group({
+
+      inChop : this.formBuilder.control(''),
+   
+      inPizzas : this.formBuilder.control(''),
+   
+      inRecheios : this.formBuilder.control(''),
+   
+      inPessoas : this.formBuilder.control('')
+   })
 
 
   }
