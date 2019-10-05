be0cc6585fb21c01d259d83dffce499a631bab06
gustavo xavier <gus.guilhermexavier08@gmail.com>
2019-10-04 22:32:25 -0300

    Exercicio-8

--- src/app/comanda-binding/comanda-binding.component.ts
+++ src/app/comanda-binding/comanda-binding.component.ts
@@ -9,23 +9,59 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class ComandaBindingComponent implements OnInit {
 
     formPizzaria: FormGroup;
-    
+ 
+
   constructor(public formBuilder   :   FormBuilder) { }
  
    
   ngOnInit() {
     this.formPizzaria = this.formBuilder.group({
 
-      inChop : this.formBuilder.control(''),
+      inChop : this.formBuilder.control('2'),
    
-      inPizzas : this.formBuilder.control(''),
+      inPizzas : this.formBuilder.control('2'),
    
-      inRecheios : this.formBuilder.control(''),
+      inRecheios : this.formBuilder.control('4'),
    
-      inPessoas : this.formBuilder.control('')
+      inPessoas : this.formBuilder.control('4'),
+
+      inServico :this.formBuilder.control(false)
+
+      
    })
 
-
   }
+   onProcessar(){
+    let chop=this.formPizzaria.value.inChop;  
+     
+    let pizza=this.formPizzaria.value.inPizzas;  
+
+    let recheios=this.formPizzaria.value.inRecheios;
+    
+    let pessoas=this.formPizzaria.value.inPessoas;  
+
+    let servico=this.formPizzaria.value.inServico;  
+    
+    let valorchop=7.30*chop;
+
+     let valorpizza=31.50*pizza;
+
+     let valorrecheio=5.90*recheios;
+
+     let valortotal = valorchop+valorpizza+valorrecheio;
+     let valortaxa = 0;
+     if (servico) {
+      valortaxa = valortotal*0.1;
+      valortotal = valortotal  + valortaxa;
+     } 
+     let valorfinal=valortotal+valortaxa;
+     let valorpessoa= valorfinal/pessoas;
+     
+
+     alert(`Consumo: R$ ${valortotal} \n Serviços: R$ ${valortaxa} \n Total: R$ ${valorfinal} \n Por Pessoa: R$ ${valorpessoa}`)
+   }
+
+
+  
 
 }
